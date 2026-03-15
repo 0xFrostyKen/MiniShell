776a955e17df4d89baf17009e575ef44437766e5
0xFrostyKen <satyal.pratik12@gmail.com>
2026-03-15 20:19:31 +0545

    working fork and command execution

---
+++ shell.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
+int main () {
+  
+  printf("\n###########################################");
+  printf("\n########### BUBU (.)(.) BUBU ##############");
+  printf("\n###########################################\n");
+  char input[MAX_INPUT];
+  char* args[MAX_ARGS];
+
+  while (1) {
+    printf("(.)(.) >");
+    fflush(stdout);
+
+    if(fgets(input, MAX_INPUT, stdin) == NULL) {
+      perror("fgets failed, it is equal to NULL");
+      continue;
+
+    }
+    
+    input[strcspn(input, "\n")] = '\0';
+  
+
+    if(strcmp(input, "exit") == 0){
+      printf("Thank you for using bubu shell all hail bubu (.)(.)");
+      break;
+    }
+
+    char* token = strtok(input, " ");
+    int i = 0;
+
+    while (token != NULL && i < MAX_ARGS) {
+      args[i++] = token;
+      token = strtok(NULL, " ");
+    }
+
+    args[i] = NULL;
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+      execvp(args[0], args);
+      perror("execvp failed");
+      exit(EXIT_FAILURE);
+
+    } else if (pid > 0 ) {
+      int status;
+      waitpid(pid, &status, 0);
+      printf("Exit status: %d \n", status);
+    } else {
+      perror("for failed maybe");
+    }
+
+  }
+  return 0;
+}
